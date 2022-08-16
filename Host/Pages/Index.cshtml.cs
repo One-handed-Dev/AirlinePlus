3f7e6b62dc565a76e75b7540e48a156e4ddbad1c
One-handed-Dev <rasoul.hosseini94@gmail.com>
2022-08-16 18:46:57 +0430

    Host: Added.

--- Host/Pages/Index.cshtml.cs
+++ Host/Pages/Index.cshtml.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AirlinePlus.Pages
+namespace Host.Pages
 {
     public class IndexModel : PageModel
     {
--- Host/Program.cs
+++ Host/Program.cs
@@ -1,13 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace AirlinePlus
+namespace Host
 {
     public class Program
     {
@@ -17,7 +11,7 @@ namespace AirlinePlus
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
